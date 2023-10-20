7e7f826e3cac2697da90910706006ecd0ef45d44
CyberNett <110544673+Cyber-Nett@users.noreply.github.com>
2023-10-20 19:45:31 +0300

    gg

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,17 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Coming Soon!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          клік! {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Find out about me on my <a href='github.com/CyberNett'>github page</a>
       </p>
     </div>
   )
